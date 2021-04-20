3a17ea8028c266a9f155de3bf83e6c506e4fb8bc
joelgnanasekaram <gnanasej@sheridancollege.ca>
2021-04-20 19:29:57 -0400

    Implemented getting all user dating profiles, regardless of city, code in the GET API endpoint of the Fictional Dating App Web API

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
 // import express
 const express = require("express");
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 // import the list of profiles
-let profilesList = require("./data.js")
-
+let profilesList = require("./data.js");
 
 // a test endpoint - not part of the assessment
 app.get("/", (req, res) => {
-    res.status(200).send({msg:"Welcome to our page!"})
-})
+    res.status(200).send({ msg: "Welcome to our page!" });
+});
 
 // @TODO: Implement the API's GET endpoint
-app.get("/api/profiles/:city?", (req, res)=>{    
+app.get("/api/profiles/:city?", (req, res) => {
     // example of accessing a single person in the profilesList
-    res.send(profilesList[0])
-})
+    if (req.params.city == null) {
+        res.status(200).send(profilesList);
+    } 
+    else if (req.params.city != null) {
+    } 
+    else {
+        const message = {
+            statusCode:404,
+            message:"No Dating Profiles found in data list"
+        }
+        res.status(404).send(message);
+    }
+});
 
 // @TODO: Implement the API's POST endpoint
-app.post("/api/profiles", (req, res) => {
-    
-})
+app.post("/api/profiles", (req, res) => {});
 
 // @TODO: Impelement the API's DELETE endpoint
-app.delete("/api/profiles", (req, res)=> {
- 
-})
+app.delete("/api/profiles", (req, res) => {});
 
 // anyone who imports this file will be able to use the "app" variable
-module.exports = app
+module.exports = app;
